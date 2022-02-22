65fd53313ab97d5d939fb07f5a910d490067ccc1
Rares Diaconu <98269458+raresdiaconu@users.noreply.github.com>
2022-02-22 23:42:19 +0200

    restart button now working. cleaned up code.

--- javascript.js
+++ javascript.js
@@ -3,53 +3,48 @@ let computerScore = 0;
 let gamesCount = 0;
 const selection = document.querySelectorAll('.icon');
 const roundWinner = document.querySelector('.announce-winner');
-const score = document.querySelector(".keep-score")
-
-function computerPlay() {
-    const computerMoves = ["Rock", "Paper", "Scissors"];
-    let randomIndex = Math.floor(Math.random() * computerMoves.length);
-    return computerMoves[randomIndex];
-}
+const score = document.querySelector('.keep-score')
+const restartButton = document.querySelector('.restart-game')
 
 selection.forEach(choice => choice.addEventListener('click', startGame));
 function startGame(e) {
-    const sel = document.querySelector(`i[id = "${e.target.id}"]`)
-    sel.classList.add("icon-active");
+    const sel = document.querySelector(`i[id = '${e.target.id}']`)
+    sel.classList.add('icon-active');
     playRound(e.target.id);
     keepScore();
+    activateRestartButton();
     if (gamesCount === 5) {
-        game();
+        announceGameWinner();
         selection.forEach(choice => choice.removeEventListener('click', startGame));
     };
 }
 
-//Removes the styling of the selection after the button has been clicked.
-selection.forEach(choice => choice.addEventListener('transitionend', removeTransition));
-function removeTransition(e) {
-    if (e.propertyName !== "transform") return;
-    this.classList.remove("icon-active");
+function computerPlay() {
+    const computerMoves = ['rock', 'paper', 'scissors'];
+    let randomIndex = Math.floor(Math.random() * computerMoves.length);
+    return computerMoves[randomIndex];
 }
 
 function playRound(playerSelection) {
     const computerSelection = computerPlay();
     switch (true) {
-        case (playerSelection === "rock" && computerSelection === "Rock"):
-        case (playerSelection === "paper" && computerSelection === "Paper"):
-        case (playerSelection === "scissors" && computerSelection === "Scissors"):
-            roundWinner.textContent = "It's a tie!";
+        case (playerSelection === 'rock' && computerSelection === 'rock'):
+        case (playerSelection === 'paper' && computerSelection === 'paper'):
+        case (playerSelection === 'scissors' && computerSelection === 'scissors'):
+            roundWinner.textContent = 'It\'s a tie!';
             gamesCount++;
             break;
-        case (playerSelection === "rock" && computerSelection === "Scissors"):
-        case (playerSelection === "paper" && computerSelection === "Rock"):
-        case (playerSelection === "scissors" && computerSelection === "Paper"):
-            roundWinner.textContent = "Good pick! You won!";
+        case (playerSelection === 'rock' && computerSelection === 'scissors'):
+        case (playerSelection === 'paper' && computerSelection === 'rock'):
+        case (playerSelection === 'scissors' && computerSelection === 'paper'):
+            roundWinner.textContent = 'Good pick! You won!';
             playerScore++;
             gamesCount++;
             break;
-        case (playerSelection === "scissors" && computerSelection === "Rock"):
-        case (playerSelection === "rock" && computerSelection === "Paper"):
-        case (playerSelection === "paper" && computerSelection === "Scissors"):
-            roundWinner.textContent = "Shucks! The computer won.";
+        case (playerSelection === 'scissors' && computerSelection === 'rock'):
+        case (playerSelection === 'rock' && computerSelection === 'paper'):
+        case (playerSelection === 'paper' && computerSelection === 'scissors'):
+            roundWinner.textContent = 'Shucks! The computer won.';
             computerScore++;
             gamesCount++;
             break;
@@ -60,12 +55,45 @@ function keepScore() {
     score.textContent = `You ${playerScore} - ${computerScore} Computer`;
 }
 
-function game() {
+function announceGameWinner() {
+    roundWinner.classList.add('announce-game-winner');
     if (playerScore > computerScore) {
-        roundWinner.textContent = "Congrats! You beat the computer!";
+        roundWinner.textContent = 'Congrats! You beat the computer!';
     } else if (playerScore < computerScore) {
-        roundWinner.textContent = "You lost. Play another round?";
+        roundWinner.textContent = 'You lost. Play another round?';
     } else {
-        roundWinner.textContent = "It's a tie. Play another round?";
+        roundWinner.textContent = 'It\'s a tie. Play another round?';
     }
+}
+
+function activateRestartButton() {
+    if (gamesCount > 0) {
+        restartButton.classList.add('restart-game-whileplaying');
+    }
+}
+
+restartButton.addEventListener('click', restartGame);
+function restartGame() {
+    if (gamesCount > 0) restartButton.classList.add('restart-game-click');
+    resetGame();
+    selection.forEach(choice => choice.addEventListener('click', startGame));
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    gamesCount = 0;
+    roundWinner.textContent = 'Make a selection to start the game:';
+    score.textContent = 'You\'re playing Best Of Five.';
+    restartButton.classList.remove('restart-game-whileplaying');
+    roundWinner.classList.remove('announce-game-winner');
+}
+
+//Removes the styling of the buttons after they have been clicked.
+selection.forEach(choice => choice.addEventListener('transitionend', removeTransition));
+restartButton.addEventListener('transitionend', removeTransition);
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('icon-active');
+    this.classList.remove('restart-game-click');
 }
